a88ca124040171c66871630087fd7b7675f0c064
StefanoDPCarraro <stefano.c@edu.pucrs.br>
2024-03-23 01:29:12 -0300

    Added the method possible moves and the validation for the move piece method based on the possible moves, possible moves correct logic not implemented yet though

--- board/Piece.java
+++ board/Piece.java
@@ -1,6 +1,6 @@
 package board;
 
-public class Piece {
+public abstract class Piece {
 
     private Position position;
     private final Board board;
@@ -20,5 +20,23 @@ public class Piece {
 
     protected void setPosition(Position position) {
         this.position = position;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
