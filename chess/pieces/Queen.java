708f06f5ddd15ce25873ba8e751ada23a96ac06b
StefanoDPCarraro <stefano.c@edu.pucrs.br>
2024-03-23 21:41:22 -0300

    Added queen with its possible moves, chess board starting position now correct only missing special moves

---
+++ chess/pieces/Queen.java
@@ -0,0 +1,101 @@
+package chess.pieces;
+
+import board.Board;
+import board.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        //MARKS AS TRUE THE POSSIBLE POSITIONS ABOVE THE QUEEN
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS TO THE LEFT OF THE QUEEN
+        p.setValues(getPosition().getRow(), getPosition().getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS BELOW THE QUEEN
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS TO THE RIGHT OF THE QUEEN
+        p.setValues(getPosition().getRow(), getPosition().getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //MARKS AS TRUE THE POSSIBLE POSITIONS NORTHWEST
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS NORTHEAST
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS SOUTHEAST
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() + 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        //MARKS AS TRUE THE POSSIBLE POSITIONS TO THE RIGHT OF THE ROOK
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + 1, p.getColumn() - 1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+
+}
