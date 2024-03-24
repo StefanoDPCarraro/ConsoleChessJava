5f5f00f907bcdb4c851cc882b979231dc4fa5afb
StefanoDPCarraro <stefano.c@edu.pucrs.br>
2024-03-23 22:11:02 -0300

    Added first special move castling, already usable and correct logic

--- chess/pieces/King.java
+++ chess/pieces/King.java
@@ -2,12 +2,15 @@ package chess.pieces;
 
 import board.Board;
 import board.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -18,6 +21,12 @@ public class King extends ChessPiece {
     private boolean canMove(Position position){
         ChessPiece p = (ChessPiece) getBoard().getPiece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece) getBoard().getPiece(position);
+        return p instanceof Rook
+                && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -64,6 +73,33 @@ public class King extends ChessPiece {
         if(getBoard().positionExists(p) && canMove(p)){
             mat[p.getRow()][p.getColumn()] = true;
         }
+
+        //SPECIAL MOVE(CASTLE)
+        if(getMoveCount() == 0 && !chessMatch.getCheck()){
+            // CASTLING KING SIDE ROOK
+            Position pRook1 = new Position(getPosition().getRow(), getPosition().getColumn() + 3);
+            if(testRookCastling(pRook1)){
+                //ADJACENT POSITIONS TEST
+                Position p1 = new Position(getPosition().getRow(), getPosition().getColumn() + 1);
+                Position p2 = new Position(getPosition().getRow(), getPosition().getColumn() + 2);
+                if(getBoard().getPiece(p1) == null && getBoard().getPiece(p2) == null){
+                    mat[getPosition().getRow()][getPosition().getColumn() + 2] = true;
+                }
+            }
+            // CASTLING QUEEN SIDE ROOK
+            Position pRook2 = new Position(getPosition().getRow(), getPosition().getColumn() - 4);
+            if(testRookCastling(pRook1)){
+                //ADJACENT POSITIONS TEST
+                Position p1 = new Position(getPosition().getRow(), getPosition().getColumn() - 1);
+                Position p2 = new Position(getPosition().getRow(), getPosition().getColumn() - 2);
+                Position p3 = new Position(getPosition().getRow(), getPosition().getColumn() - 3);
+                if(getBoard().getPiece(p1) == null && getBoard().getPiece(p2) == null
+                        && getBoard().getPiece(p3) == null){
+                    mat[getPosition().getRow()][getPosition().getColumn() - 2] = true;
+                }
+            }
+        }
+
         return mat;
     }
 }
